54219da01a24ebef156731fd85baf5a7d7f5e1ee
Alessandro Ferrucci <alessandroferrucci@gmail.com>
2017-09-14 10:57:22 -0400

    - finalized load of CPSC data

--- RecallsGlobals/src/main/java/recallsapi2/recallsglobals/GlobalDataAccess.java
+++ RecallsGlobals/src/main/java/recallsapi2/recallsglobals/GlobalDataAccess.java
@@ -17,17 +17,17 @@ public class GlobalDataAccess extends BasicJdbcSupport {
     //private DataSource ds;
     private BoneCP connectionPool = null;
 
-    protected String RECALLS_COLS = "ID,SOURCE_ORGANIZATION,RECALLS_ID,RECALL_NUMBER,RECALL_REASON,RECALL_STATUS,RECALL_DATE,LAST_PUBLISH_DATE,RECALL_DESCRIPTION,RECALL_URL,RECALL_CLASSIFICATION,RECALL_VOLUNTARY_MANDATED,CONSUMER_CONTACT,REPORTED_COMPLAINTS,REMEDIES";
+    protected String RECALLS_COLS = "ID,SOURCE_ORGANIZATION,RECALL_ID,RECALL_NUMBER,RECALL_REASON,RECALL_STATUS,RECALL_DATE,LAST_PUBLISH_DATE,RECALL_DESCRIPTION,RECALL_URL,RECALL_CLASSIFICATION,RECALL_VOLUNTARY_MANDATED,CONSUMER_CONTACT,REPORTED_COMPLAINTS,REMEDIES";
     protected String PRODUCTS_COLS = "RECALLS_ID,UPCS,NAME,DESCRIPTION,MODEL,TYPE,CATEGORY_ID,IMAGES,PRODUCT_QUANTITY";
-    protected String MANUFACTURER_COLS = "ID,RECALLS_ID,NAME";
+    protected String MANUFACTURER_COLS = "ID,RECALLS_ID,NAME,COMPANY_ID";
     protected String MANUFACTURER_LOCATIONS_COLS = "MANUFACTURERS_ID,COUNTRY,STATE,CITY";
     protected String HAZARDS_COLS = "RECALLS_ID,HAZARD,HAZARD_TYPE_ID";
     protected String RETAILERS_COLS = "RECALLS_ID,NAME,COMPANY_ID";
 
     protected String INSERT_RECALLS_SQL = String.format("INSERT INTO RECALLS (%s) VALUES (%s)", RECALLS_COLS, GeneralUtils.multiplyStringWithToken("?", ",", RECALLS_COLS.split(",").length));
     protected String INSERT_PRODUCTS_SQL = String.format("INSERT INTO PRODUCTS (%s) VALUES (%s)", PRODUCTS_COLS, GeneralUtils.multiplyStringWithToken("?", ",", PRODUCTS_COLS.split(",").length));
-    protected String INSERT_MANUFACTURER_SQL = String.format("INSERT INTO MANUFACTURER (%s) VALUES (%s)", MANUFACTURER_COLS, GeneralUtils.multiplyStringWithToken("?", ",", MANUFACTURER_COLS.split(",").length));
-    protected String INSERT_MANUFACTURER_LOCATIONS_SQL = String.format("INSERT INTO MANUFACTURER (%s) VALUES (%s)", MANUFACTURER_LOCATIONS_COLS, GeneralUtils.multiplyStringWithToken("?", ",", MANUFACTURER_LOCATIONS_COLS.split(",").length));
+    protected String INSERT_MANUFACTURER_SQL = String.format("INSERT INTO MANUFACTURERS (%s) VALUES (%s)", MANUFACTURER_COLS, GeneralUtils.multiplyStringWithToken("?", ",", MANUFACTURER_COLS.split(",").length));
+    protected String INSERT_MANUFACTURER_LOCATIONS_SQL = String.format("INSERT INTO MANUFACTURERS_LOCATIONS (%s) VALUES (%s)", MANUFACTURER_LOCATIONS_COLS, GeneralUtils.multiplyStringWithToken("?", ",", MANUFACTURER_LOCATIONS_COLS.split(",").length));
     protected String INSERT_HAZARDS_SQL = String.format("INSERT INTO HAZARDS (%s) VALUES (%s)", HAZARDS_COLS, GeneralUtils.multiplyStringWithToken("?", ",", HAZARDS_COLS.split(",").length));
     protected String INSERT_RETAILERS_SQL = String.format("INSERT INTO RETAILERS (%s) VALUES (%s)", RETAILERS_COLS, GeneralUtils.multiplyStringWithToken("?", ",", RETAILERS_COLS.split(",").length));
 
